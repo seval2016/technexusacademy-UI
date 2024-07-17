a00812693a01dbe39bb70265565d5b7cce1f56e8
seval2016 <sevalsenturk2016@gmail.com>
2024-07-17 19:06:11 +0300

    header - navbar

---
+++ src/components/common/header/logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavbarBrand } from "react-bootstrap";
+import { Link } from "next/navigation";
+import Image from "next/image";
+import { config } from "@/helpers/config";
+
+const Logo = ({ theme = "dark" }) => {
+	return (
+		<NavbarBrand href="/" as={Link}>
+			<Image
+				src={`/img/logos/logo-${theme}.png`}
+				alt={config.project.name}
+				width={200}
+				height={57}
+			/>
+		</NavbarBrand>
+	);
+};
+
+export default Logo;
